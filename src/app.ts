f7d90ec4c165bdbf738b0692caaa13e1ad734d3b
Farok Hossain <farokafs7@gmail.com>
2024-08-12 00:02:17 +0600

    schema created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import { Schema } from "mongoose";
 
 const app: Application = express();
 
@@ -18,6 +19,9 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
     step4: Database Query
     */
 
+  //   res.send("Hello Worldd!");
+  //   next();
+
   interface IUser {
     id: string;
     role: "student";
@@ -35,8 +39,28 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
     parmanentAddress: string;
   }
 
-  //   res.send("Hello Worldd!");
-  //   next();
+  const userSchema = new Schema<IUser>({
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    dateOfBirth: { type: String },
+    gender: { type: String, enum: ["male", "female"] },
+    email: { type: String },
+    contactNo: { type: String, required: true },
+    emergencyContactNo: { type: String, required: true },
+    presentAddress: { type: String, required: true },
+    parmanentAddress: { type: String, required: true },
+  });
 });
 
 export default app;
