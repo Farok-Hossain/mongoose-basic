e855a66ee4e913c248cbd3b72c03fe76e6297326
Farok Hossain <farokafs7@gmail.com>
2024-08-11 23:44:32 +0600

    interface created

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,38 @@ const app: Application = express();
 
 app.use(cors());
 
+// parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello Worldd!");
-  next();
+  // inseting a test data into mongodb
+  /*
+    step1: Interface
+    step2: Schema
+    step3: Model
+    step4: Database Query
+    */
+
+  interface IUser {
+    id: string;
+    role: "student";
+    password: string;
+    name: {
+      firstName: string;
+      lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    presentAddress: string;
+    parmanentAddress: string;
+  }
+
+  //   res.send("Hello Worldd!");
+  //   next();
 });
 
 export default app;
