e12d2a1b126956073104cc27cb0993913214a1c5
Farok Hossain <farokafs7@gmail.com>
2024-08-12 22:53:20 +0600

    user.model created

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+import { IUser } from "./user.interface";
+
+// creating schema
+const userSchema = new Schema<IUser>({
+  id: { type: String, required: true, unique: true },
+  role: { type: String, required: true },
+  password: { type: String, required: true },
+  name: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  dateOfBirth: { type: String },
+  gender: { type: String, enum: ["male", "female"] },
+  email: { type: String },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  presentAddress: { type: String, required: true },
+  parmanentAddress: { type: String, required: true },
+});
+
+// creating model
+const User = model<IUser>("User", userSchema);
